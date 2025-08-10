1088c5ffe747e24618a7985b01b81f0cfd64f1d1
Maiabbass <maimhrezalabbass800@gmail.com>
2025-08-10 13:10:14 +0300

    Host2

--- supermarket/settings.py
+++ supermarket/settings.py
@@ -127,7 +127,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+#STATIC_URL = "static/"
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -165,3 +168,9 @@ DATABASES = {
         'PORT': '5432',                         # البورت
     }
 }
+
+
+
+#Host
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
